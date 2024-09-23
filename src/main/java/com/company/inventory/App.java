5d94d55f27d361c7d643a1477938bc366fc93797
bustedelbow <velozity2@gmail.com>
2024-09-23 19:27:02 +0800

    Added FontAwesome dependency

--- src/main/java/com/company/inventory/App.java
+++ src/main/java/com/company/inventory/App.java
@@ -8,9 +8,12 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Root extends Application {
+
+    private Db db = new Db();
     @Override
     public void start(Stage stage) throws Exception{
-        Image icon = new Image(getClass().getResource("/images/um_logo.jpg").toString());
+        db.getConnection();
+        Image icon = new Image(getClass().getResource("/icons/um_logo.jpg").toString());
 
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/root_layout.fxml"));
         Scene scene = new Scene(root);
@@ -20,6 +23,8 @@ public class Root extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+
+//        db.getConnection();
     }
 
     public static void main(String[] args) {
