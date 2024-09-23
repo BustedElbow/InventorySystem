095701857e35b5397f52b595640e360a3d31052b
bustedelbow <velozity2@gmail.com>
2024-09-23 19:27:36 +0800

    Updated module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.example.inventorysystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+    requires de.jensd.fx.glyphs.fontawesome;
 
 
     opens com.company.inventory.controllers to javafx.fxml;
     exports com.company.inventory;
+    exports com.company.inventory.controllers;
+    exports com.company.inventory.models;
+    exports com.company.inventory.views;
 }
