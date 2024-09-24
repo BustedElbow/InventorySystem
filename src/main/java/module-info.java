e269855b9357fc1bbff67eb8ab287b96c55cf86d
bustedelbow <velozity2@gmail.com>
2024-09-24 23:17:05 +0800

    Fixed missing export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,10 @@ module com.example.inventorysystem {
     requires javafx.fxml;
     requires java.sql;
     requires org.xerial.sqlitejdbc;
-    requires de.jensd.fx.glyphs.fontawesome;
 
 
     opens com.company.inventory.controllers to javafx.fxml;
     exports com.company.inventory;
     exports com.company.inventory.controllers;
     exports com.company.inventory.models;
-    exports com.company.inventory.views;
 }
