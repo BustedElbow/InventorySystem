8fef2e6576c78cb560186c4f91338a3d9587b9a4
bustedelbow <velozity2@gmail.com>
2024-09-25 23:35:40 +0800

    Controller: Re: Add item functionality

--- src/main/java/com/company/inventory/controllers/ItemModalController.java
+++ src/main/java/com/company/inventory/controllers/ItemModalController.java
@@ -1,15 +1,29 @@
 package com.company.inventory.controllers;
 
+import com.company.inventory.models.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
 public class ItemModalController {
 
+
+    @FXML private ChoiceBox<String> unitMeasureChoices;
+    @FXML private TextField itemName;
+    @FXML private TextField stockQuantity;
+    @FXML private TextField reorderLevel;
     @FXML private Button cancelBtn;
     @FXML private Button confirmBtn;
+
+    @FXML
+    private void initialize() {
+        unitMeasureChoices.getItems().addAll("Kilograms", "Liters", "Pieces");
+        unitMeasureChoices.setValue("Kilograms");
+    }
 
     @FXML
     private void btnCancel(ActionEvent event) {
@@ -19,7 +33,16 @@ public class ItemModalController {
 
     @FXML
     private void btnConfirm(ActionEvent event) {
-        System.out.println("Item Added... buhh");
+
+        String name = itemName.getText();
+        double stock = Double.parseDouble(stockQuantity.getText());
+        double reorder = Double.parseDouble(reorderLevel.getText());
+        String unit = unitMeasureChoices.getValue();
+
+        Item newItem = new Item(name, unit, stock, reorder);
+        newItem.save();
+
+        System.out.println(newItem.getName() + " was added to the inventory with a ID of " + newItem.getId());
 
         Stage stage = (Stage) confirmBtn.getScene().getWindow();
         stage.close();
