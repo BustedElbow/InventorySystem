2a0d3aab6176293deaad0ea040eed3dec4e1a1b6
bustedelbow <velozity2@gmail.com>
2024-09-25 22:42:08 +0800

    Model: Item save method & set ID

--- src/main/java/com/company/inventory/models/Item.java
+++ src/main/java/com/company/inventory/models/Item.java
@@ -1,5 +1,11 @@
 package com.company.inventory.models;
 
+import com.company.inventory.SQLiteDatabase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Item {
     private int id;
     private String name;
@@ -7,12 +13,15 @@ public class Item {
     private double stock;
     private double reorderLevel;
 
-    public Item(int id, String name, String unit, double stock, double reorderLevel) {
-        this.id = id;
+    public Item(String name, String unit, double stock, double reorderLevel) {
         this.name = name;
         this.unitMeasure = unit;
         this.stock = stock;
         this.reorderLevel = reorderLevel;
+    }
+
+    private void setId(int id) {
+        this.id = id;
     }
 
     public void setUnitMeasure(String unit) {
@@ -32,18 +41,45 @@ public class Item {
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getUnitMeasure() {
-        return unitMeasure;
+        return this.unitMeasure;
     }
 
     public double getStock() {
-        return stock;
+        return this.stock;
     }
 
     public double getReorderLevel() {
-        return reorderLevel;
+        return this.reorderLevel;
+    }
+
+    public void save() {
+        String query = "INSERT INTO items(item_name, unit_measure, stock_quantity, reorder_level) VALUES(?, ?, ?, ?)";
+
+        try(Connection conn = SQLiteDatabase.connect();
+        PreparedStatement ps = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1, this.name);
+            ps.setString(2, this.unitMeasure);
+            ps.setDouble(3, this.stock);
+            ps.setDouble(4, this.reorderLevel);
+            int affectedRows = ps.executeUpdate();
+
+            if (affectedRows > 0) {
+                try(ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        int id = rs.getInt(1);
+                        setId(id);
+                        System.out.println("Item inserted successfully");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
