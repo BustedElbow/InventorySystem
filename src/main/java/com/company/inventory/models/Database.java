161b1a7400da8b3f2bfad5d8a1cbe287649a02e8
bustedelbow <velozity2@gmail.com>
2024-09-26 22:37:43 +0800

    Models: Created database model

---
+++ src/main/java/com/company/inventory/models/Database.java
@@ -0,0 +1,48 @@
+package com.company.inventory.models;
+
+import com.company.inventory.SQLiteDatabase;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Database {
+
+    private static ObservableList<String> itemList = FXCollections.observableArrayList();
+
+    private static void loadItemsFromDatabase() {
+        String query = "SELECT * FROM items";
+
+        try(Connection conn = SQLiteDatabase.connect();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                int itemId = rs.getInt("item_id");
+                String itemName = rs.getString("item_name");
+                String unitMeasure = rs.getString("unit_measure");
+                double itemStock = rs.getDouble("stock_quantity");
+                double itemLevel = rs.getDouble("reorder_level");
+
+                String listItem = String.format("ID: %d | Name: %s | Unit: %s | Stock: %f | Reorder Level: %f", itemId, itemName, unitMeasure, itemStock, itemLevel);
+
+                itemList.add(listItem);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static ObservableList<String> getItemList() {
+        loadItemsFromDatabase();
+        return itemList;
+    }
+
+    public static void refreshItemList() {
+        itemList.clear();
+        loadItemsFromDatabase();
+    }
+}
