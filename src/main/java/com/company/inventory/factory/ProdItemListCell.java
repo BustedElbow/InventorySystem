fe043938b807902c4ea49abd23d0888109245028
bustedelbow <velozity2@gmail.com>
2024-10-02 17:30:49 +0800

    Updated look of customCell: ProdItemList & ProdUsedItemList

--- src/main/java/com/company/inventory/factory/ProdItemListCell.java
+++ src/main/java/com/company/inventory/factory/ProdItemListCell.java
@@ -6,6 +6,8 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
@@ -14,7 +16,7 @@ public class ProdItemListCell extends ListCell<Item> {
     private StackPane itemPane = new StackPane();
     private StackPane actionPane = new StackPane();
     private Label name = new Label();
-    private Button addButton = new Button("->");
+    private Button addButton = new Button();
     private AddProductController controller;
     public ProdItemListCell(AddProductController controller) {
         this.controller = controller;
@@ -22,13 +24,22 @@ public class ProdItemListCell extends ListCell<Item> {
         itemPane.setAlignment(Pos.CENTER_LEFT);
         actionPane.setAlignment(Pos.CENTER_RIGHT);
 
+        Image image = new Image(getClass().getResource("/icons/add100-black.png").toString());
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(20);
+
+        addButton.setGraphic(imageView);
+        addButton.setStyle("-fx-background-color: #ee8850; -fx-background-radius: 4; -fx-cursor: hand;");
+
         itemPane.getChildren().add(name);
         actionPane.getChildren().add(addButton);
 
-        itemPane.setPrefWidth(100);
-        actionPane.setPrefWidth(100);
+        itemPane.setPrefWidth(224);
+        actionPane.setPrefWidth(190);
 
-        hbox.setPrefWidth(200);
+        hbox.setPrefWidth(364);
         hbox.getChildren().addAll(itemPane, actionPane);
 
         setGraphic(hbox);
--- src/main/java/com/company/inventory/factory/ProdUsedItemListCell.java
+++ src/main/java/com/company/inventory/factory/ProdUsedItemListCell.java
@@ -6,6 +6,8 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Label;
@@ -20,7 +22,7 @@ public class ProdUsedItemListCell extends ListCell<Item> {
     private Label name = new Label();
     private Label measure = new Label();
     private TextField quantity = new TextField();
-    private Button removeButton = new Button("<-");
+    private Button removeButton = new Button();
     private Item item;
 
     public ProdUsedItemListCell(AddProductController controller) {
@@ -30,11 +32,33 @@ public class ProdUsedItemListCell extends ListCell<Item> {
         itemPane.setAlignment(Pos.CENTER_LEFT);
         measurePane.setAlignment(Pos.CENTER_RIGHT);
 
+        Image image = new Image(getClass().getResource("/icons/minus100-black.png").toString());
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(20);
+
+        removeButton.setGraphic(imageView);
+        removeButton.setStyle("-fx-background-color: #ee8850; -fx-background-radius: 4; -fx-cursor: hand;");
+
+        quantity.setStyle("-fx-text-fill: #1e1e1e;\n" +
+                "    -fx-font-family: \"Inter Semi Bold\";\n" +
+                "    -fx-font-size: 16;\n" +
+                "    -fx-border-color: #b8b8b8;\n" +
+                "    -fx-border-radius: 8;\n" +
+                "    -fx-background-radius: 8;");
+
         actionPane.getChildren().add(removeButton);
         itemPane.getChildren().add(name);
         quantityPane.getChildren().add(quantity);
         measurePane.getChildren().add(measure);
 
+        actionPane.setPrefWidth(67);
+        itemPane.setPrefWidth(200);
+        quantityPane.setPrefWidth(89);
+        measurePane.setPrefWidth(96);
+
+        hbox.setPrefWidth(458);
         hbox.getChildren().addAll(actionPane, itemPane, quantityPane, measurePane);
 
         setGraphic(hbox);
@@ -43,9 +67,9 @@ public class ProdUsedItemListCell extends ListCell<Item> {
             if (item != null) {
                 try {
                     double newQuantity = Double.parseDouble(newText);
-                    controller.updateItemQuantity(item, newQuantity); // Update the map
+                    controller.updateItemQuantity(item, newQuantity);
                 } catch (NumberFormatException e) {
-                    controller.updateItemQuantity(item, 0.0); // Default to 0 if input is invalid
+                    controller.updateItemQuantity(item, 0.0);
                 }
             }
         });
@@ -69,11 +93,4 @@ public class ProdUsedItemListCell extends ListCell<Item> {
         }
     }
 
-    public TextField getQuantity() {
-        return quantity;
-    }
-
-    public Item getUsedItem() {
-        return item;
-    }
 }
