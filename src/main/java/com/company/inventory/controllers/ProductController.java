1d4ea7a3b70ab22af17926fe3d7931741f3b604a
bustedelbow <velozity2@gmail.com>
2024-10-02 12:42:14 +0800

    Custom listcell for ProductList

--- src/main/java/com/company/inventory/controllers/ProductController.java
+++ src/main/java/com/company/inventory/controllers/ProductController.java
@@ -1,14 +1,30 @@
 package com.company.inventory.controllers;
 
+import com.company.inventory.factory.ProductListCell;
+import com.company.inventory.models.Database;
+import com.company.inventory.models.Product;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ProductController {
 
+    @FXML private ListView<Product> productListView;
+
+    @FXML
+    public void initialize() {
+        ObservableList<Product> products = Database.getProductList();
+        productListView.setItems(products);
+
+        productListView.setCellFactory(param -> new ProductListCell());
+
+    }
 
     public void showModal() {
         try {
---
+++ src/main/java/com/company/inventory/factory/ProductListCell.java
@@ -0,0 +1,51 @@
+package com.company.inventory.factory;
+
+import com.company.inventory.models.Product;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+public class ProductListCell extends ListCell<Product> {
+    private HBox hbox = new HBox();
+    private StackPane productPane = new StackPane();
+    private StackPane pricePane = new StackPane();
+    private Label productId = new Label();
+    private Label productName = new Label();
+    private Label productPrice = new Label();
+
+    public ProductListCell() {
+
+        productPane.setAlignment(Pos.CENTER_LEFT);
+        pricePane.setAlignment(Pos.CENTER_RIGHT);
+
+        productId.setVisible(false);
+        productId.setManaged(false);
+
+        productPane.getChildren().addAll(productId, productName);
+        pricePane.getChildren().add(productPrice);
+
+        productPane.setPrefWidth(265);
+        pricePane.setPrefWidth(265);
+
+        hbox.setPrefWidth(530);
+        hbox.getChildren().addAll(productPane, pricePane);
+
+        setGraphic(hbox);
+    }
+
+    @Override
+    protected void updateItem(Product product, boolean empty) {
+        super.updateItem(product, empty);
+        if(empty || product == null) {
+            setGraphic(null);
+        } else {
+            productName.setText(product.getProductName());
+            productId.setText(Integer.toString(product.getProductId()));
+            productPrice.setText(Double.toString(product.getProductPrice()));
+
+            setGraphic(hbox);
+        }
+    }
+}
